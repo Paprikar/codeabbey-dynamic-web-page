9d4f6180ca9e65ecada2ba7253b4d10db7f3d89f
Paprikar <40892869+Paprikar@users.noreply.github.com>
2021-01-12 05:12:37 +0300

    Create task.js

---
+++ task.js
@@ -0,0 +1,6 @@
+var secret = 1753;
+var msg = "Random value is ";
+min = Math.ceil(10000 / secret);
+max = Math.floor(99999999 / secret);
+ans = (Math.floor(Math.random() * (max - min)) + min) * secret;
+document.getElementById("task").innerHTML = msg + ans;
