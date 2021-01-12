ce99bfc100b2a5a296ea1d08838fdd36fdaf7a2d
Paprikar <40892869+Paprikar@users.noreply.github.com>
2021-01-12 05:36:19 +0300

    Create task.py

---
+++ task.py
@@ -0,0 +1,9 @@
+import random
+x=1397
+while True:
+    Nilai_Acak=random.randint(10000,99999999)
+    if (Nilai_Acak%x)==0:
+        break
+print("Content-Type: text/plain")
+print("")
+print("Random value is " + str(Nilai_Acak));
